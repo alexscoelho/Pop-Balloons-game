5dae7ff6189b1dee7a35cdf5e6045b91ab25e5d6
alexscoelho <alexsoncoelho21@hotmail.com>
2021-02-20 22:04:32 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,61 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  balloonHandler();
+};
+
+let colors = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "pink",
+  "grey",
+  "purple",
+  "black",
+  "green",
+  "brown",
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "pink",
+  "grey",
+  "purple",
+  "black",
+  "green",
+  "brown"
+];
+
+let balloonContainer = document.querySelector("#ballooon-map");
+
+const popBalloon = balloonPosition => {
+  colors[balloonPosition] = null;
+  balloonHandler();
+  gameOver();
+};
+
+const balloonHandler = () => {
+  let content = "";
+  colors.forEach((elem, index) => {
+    content += `<div class="balloon ${
+      elem === null ? "popped" : "active"
+    } ${elem}"></div>`;
+  });
+
+  balloonContainer.innerHTML = content;
+
+  if (content !== "") {
+    let balloons = document.querySelectorAll(".balloon");
+    balloons.forEach((balloon, index) =>
+      balloon.addEventListener("click", () => popBalloon(index))
+    );
+  }
+};
+
+const gameOver = () => {
+  const allNull = colors.every(elem => elem === null);
+  if (allNull) {
+    balloonContainer.innerHTML = "<h1>Game Over</h1>";
+  }
 };
